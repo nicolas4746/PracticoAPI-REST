a18a645326a1c6824abc720983e49d0fd89fb2dd
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-07-27 11:43:24 -0300

    clase 15

--- src/main.js
+++ src/main.js
@@ -14,7 +14,10 @@ function createMovies(movies, container) {
     movies.forEach(movie =>{
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
-        
+        movieContainer.addEventListener('click', () => {
+            location.hash = `#movie=${movie.id}`;
+        });// evento para ir a movie detail page de cada pelicula.
+
         const movieImg = document.createElement('img');
         movieImg.classList.add('movie-img');
 
@@ -89,3 +92,25 @@ async function getTrendingMovies() {
     const movies = data.results;
     createMovies(movies, genericSection);
 };
+
+async function getMovieById(id) {
+    const {data:movie} = await api(`movie/${id}`);
+    
+    const movieImgUrl = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+
+    headerSection.style.background = `
+    linear-gradient(  
+        180deg,
+        rgba(0, 0, 0, 0.35) 19.27%,
+        rgba(0, 0, 0, 0) 29.17%
+      ), 
+    url(${movieImgUrl})
+    `;
+    // linear-gradient =>esto pone un fondo gradiente negro arriba de cada imagen,para ver mejor la flecha blanca de return.
+    movieDetailTitle.textContent = movie.title;
+    movieDetailDescription.textContent = movie.overview;
+    movieDetailScore.textContent = movie.vote_average;
+
+    createCategories(movie.genres,movieDetailCategoriesList); 
+};
+
